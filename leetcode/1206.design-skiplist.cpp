c457e745208c0ca3e45b1ffce254a21504533f51
vikashkumr <vky0018@gmail.com>
2021-06-09 23:43:37 +0530

    list update

---
+++ leetcode/1206.design-skiplist.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1206 lang=cpp
+ *
+ * [1206] Design Skiplist
+ */
+
+// @lc code=start
+class Skiplist {
+public:
+    Skiplist() {
+        
+    }
+    
+    bool search(int target) {
+        
+    }
+    
+    void add(int num) {
+        
+    }
+    
+    bool erase(int num) {
+        
+    }
+};
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * Skiplist* obj = new Skiplist();
+ * bool param_1 = obj->search(target);
+ * obj->add(num);
+ * bool param_3 = obj->erase(num);
+ */
+// @lc code=end
+
